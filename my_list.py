944c526e3701ffc95827946abf252ac13d6bcf89
Mohrgan <160346971+Mohrgan@users.noreply.github.com>
2024-03-05 21:51:39 +0300

    Initial commit

---
+++ my_list.py
@@ -0,0 +1,12 @@
+my_list = [10, 20, 30, 40]
+print(my_list)
+my_list.insert(4, 15)
+print(my_list)
+add = [50, 60, 70]
+my_list.extend(add)
+print(my_list)
+my_list.remove(70)
+print(my_list)
+my_list.sort()
+print(my_list)
+print(my_list.index(30))
